b04e73dc3f773449db2351369415b33d4c569d81
maccms <maccms@maccms.com>
2021-03-06 14:00:06 +0800

    1，新增采集资源。

--- application/extra/quickmenu.php
+++ application/extra/quickmenu.php
@@ -1,8 +1,12 @@
 <?php
-return array (
-  0 => '测试系统模块菜单,system/config',
-  1 => '测试远程地址菜单,//www.baidu.com/',
-  2 => '测试插件文件菜单,/application/xxxx.html',
-  3 => '行分隔符,###',
-  4 => '文章管理,art/data',
+return array(
+    0 => '测试系统模块菜单,system/config',
+    1 => '测试远程地址菜单,//www.baidu.com/',
+    2 => '测试插件文件菜单,/application/xxxx.html',
+    3 => '行分隔符,###',
+    4 => '文章管理,art/data',
+    5 => '行分隔符,###',
+    6 => '萌芽采集,/addons/mycj/cjv10.php',
+    7 => '行分隔符,###',
+    8 => '免费采集,/addons/cjcj/cjv10.php',
 );
